be906b7cbe85704a535bbaf8e88f5f57b43a259c
lvfelone <felone@wisc.edu>
2020-05-08 18:35:36 -0400

    Initial commit for 813 project
    -README.md
    -Rproj
    -Data synthesis script

---
+++ README.md
@@ -0,0 +1,19 @@
+This project utilizes the Correlates of War Project, a common source of data for International Relations work pertaining to conflict. 
+
+MID - militarized interstate disputes 1/1/1816 through 12/31/2010
+Hostility level 
+-1 No militarized action
+-2 Threat to use force
+-3 Display of force
+-4 Use of force
+-5 War
+
+
+
+Membership in more IGOs = less war/violent conflict
+
+# of IGOs members in vs. # of wars fought in 
+
+-Total number of memberships through the last year
+-Count of # of member years in IGO 
+
---
+++ code/Dataset synthesis.R
@@ -0,0 +1,57 @@
+
+library("here")
+library("tidyverse")
+library("broom")
+library("expss")
+
+
+# ------- make MID data set -------
+
+MID_participant <- haven::read_dta(here("data", "MIDB - participant 4.3.dta"))
+
+MID_slim <- select(MID_participant, 
+                   ccode,
+                   styear,
+                   endyear,
+                   hostlev)
+
+write_csv(MID_slim, here("data", "MID-slim-export.csv"))
+
+
+
+# ------- manipulate MID data set -------
+
+total_hostlev_all <- MID_slim %>% 
+  group_by(ccode) %>% 
+  count(hostlev) %>% 
+  mutate(total = sum(n))
+
+
+total_hostlev_45 <- MID_slim %>% 
+  filter(hostlev == 4 | hostlev == 5) %>% 
+  group_by(ccode) %>% 
+  count(hostlev) %>% 
+  mutate(total_45 = sum(n))
+
+
+
+# ------- manipulate IGO data set -------
+
+IGO <- read_csv(here("data", "IGO_state_year_formatv3.csv"))
+
+IGO_slim <- IGO %>% 
+  group_by(ccode) %>% 
+  summarize_at(
+    .vars = vars(.),
+    .funs = ~ . %in% c(1, 2)
+  )
+
+
+GAH <- IGO %>% 
+  group_by(ccode) %>% 
+  summarize_at(
+    .vars = vars(AAAID:Wassen),
+    .funs = ~ . %in% c(1, 2)
+  )
+
+
